9c4326b6e739105a0a363122fa285b66a12b996b
toniohk <toniohiraki@gmail.com>
2022-02-07 17:45:53 +0900

    feat: add the landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,99 @@
+function range(start, end, increment = 1) {
+  const count = Math.floor((end - start + increment) / increment);
+  return Array(count).fill(0).map((_, idx) => start + idx * increment);
+}
+
+const minFontSize = 2;
+const maxFontSize = 140;
+
+const minSpacingPixel = 0;
+const maxSpacingPixel = 1200;
+const spacingPixelIncrement = 2;
+
+const vhs = ['10vh', '20vh', '30vh', '40vh', '50vh', '60vh', '70vh', '80vh', '90vh', '100vh'];
+
 module.exports = {
   // Uncomment the line below to enable the experimental Just-in-Time ("JIT") mode.
   // https://tailwindcss.com/docs/just-in-time-mode
   // mode: "jit",
   theme: {
-    extend: {},
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    backgroundColor: {
+      gray: {
+        '500': '#2F333B',
+      },
+      blue: {
+        '200': '#36D4FB',
+        '300': '#0059FB',
+      },
+    },
+    colors: {
+      white: '#FFFFFF',
+      gray: {
+        '100': '#8B8B8B',
+        '200': '#646363',
+        '300': '#4B4B4B',
+      },
+      blue: {
+        '100': '#007AFF',
+      },
+    },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '6rem',
+      },
+    },
+    flex: {
+      '1': '1 1 0%',
+      '2': '2 2 0%',
+      '3': '3 3 0%',
+      '4': '4 4 0%',
+      '5': '5 5 0%',
+      'none': 'none',
+    },
+    fontFamily: {
+      primary: ['DicSans'],
+      content: ['Futura PT'],
+    },
+    fontSize: {
+      ...range(minFontSize, maxFontSize).reduce((merged, f) => ({ ...merged, [f]: `${f}px` }), {})
+    },
+    spacing: {
+      ...range(minSpacingPixel, maxSpacingPixel, spacingPixelIncrement).reduce((merged, f) => ({ ...merged, [f]: `${f}px` }), {})
+    },
+    lineHeight: {
+      ...range(minSpacingPixel, maxSpacingPixel, spacingPixelIncrement).reduce((merged, f) => ({ ...merged, [f]: `${f}px` }), {})
+    },
+    maxWidth: {
+      ...range(minSpacingPixel, maxSpacingPixel, spacingPixelIncrement).reduce((merged, f) => ({ ...merged, [f]: `${f}px` }), {})
+    },
+    minWidth: {
+      ...range(minSpacingPixel, maxSpacingPixel, spacingPixelIncrement).reduce((merged, f) => ({ ...merged, [f]: `${f}px` }), {})
+    },
+    maxHeight: {
+      ...range(minSpacingPixel, maxSpacingPixel, spacingPixelIncrement).reduce((merged, f) => ({ ...merged, [f]: `${f}px` }), {}),
+      ...vhs.reduce((merged, vh) => ({ ...merged, [vh]: vh }), {})
+    },
+    minHeight: {
+      ...range(minSpacingPixel, maxSpacingPixel, spacingPixelIncrement).reduce((merged, f) => ({ ...merged, [f]: `${f}px` }), {}),
+      ...vhs.reduce((merged, vh) => ({ ...merged, [vh]: vh }), {})
+    },
   },
   variants: {},
   plugins: [],
   purge: {
     // Filenames to scan for classes
+    enabled: false,
     content: [
       "./src/**/*.html",
       "./src/**/*.js",
